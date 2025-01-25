4cf3da3d1f3e34b54c5336a03aac3b89359adf4b
Pluto <roso3600@gmail.com>
2025-01-25 21:13:04 +0300

    hw3

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { HW3 } from './HW/3/HW3';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<HW1 />*/}
+      {/*<HW2 />*/}
+      <HW3 />
+      {/*<HW4 />*/}
+    </div>
+  );
+}
+
+export default App;
